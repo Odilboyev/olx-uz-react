f0f72f721c094a9c5cced2f85d35e9233e5456dc
Odilboyev <samandarodilboyev@gmail.com>
2021-08-05 09:27:00 +0500

    home.js updated

--- src/Data/home.js
+++ src/Data/home.js
@@ -10,9 +10,10 @@ const homedata = [
         location: "Ташкент, Юнусабадский район Вчера 12:05",
     },
     {
-        img: "https://apollo-olx.cdnvideo.ru/v1/files/bl3l7ztmi5jg1-UZ/image;s=644x461",
-        title: "Chinoz Qum, Pesok, Чиназский песок Turpoq",
-        location: "Ташкент, Юнусабадский район Вчера 12:05",
+        img: "https://apollo-olx.cdnvideo.ru/v1/files/rf068nddrpc81-UZ/image;s=644x461",
+        title: "Kredit! Poco M3 4/64",
+        price: "190 у.е.",
+        location: "Ташкент, Сергелийский район 1 авг.",
     },
     {
         img: "https://apollo-olx.cdnvideo.ru/v1/files/bl3l7ztmi5jg1-UZ/image;s=644x461",
@@ -25,9 +26,10 @@ const homedata = [
         location: "Ташкент, Юнусабадский район Вчера 12:05",
     },
     {
-        img: "https://apollo-olx.cdnvideo.ru/v1/files/bl3l7ztmi5jg1-UZ/image;s=644x461",
-        title: "Chinoz Qum, Pesok, Чиназский песок Turpoq",
-        location: "Ташкент, Юнусабадский район Вчера 12:05",
+        img: "https://apollo-olx.cdnvideo.ru/v1/files/8xs3vvm1zl5r1-UZ/image;s=644x461",
+        title: "Щенки сибирской ХАСКИ",
+        price: "200 у.е.",
+        location: "Каган Сегодня 09:55",
     },
     {
         img: "https://apollo-olx.cdnvideo.ru/v1/files/bl3l7ztmi5jg1-UZ/image;s=644x461",
@@ -40,9 +42,10 @@ const homedata = [
         location: "Ташкент, Юнусабадский район Вчера 12:05",
     },
     {
-        img: "https://apollo-olx.cdnvideo.ru/v1/files/bl3l7ztmi5jg1-UZ/image;s=644x461",
-        title: "Chinoz Qum, Pesok, Чиназский песок Turpoq",
-        location: "Ташкент, Юнусабадский район Вчера 12:05",
+        img: "https://apollo-olx.cdnvideo.ru/v1/files/itjcj6ecf33z2-UZ/image;s=644x461",
+        title: "Labo 2021 Rangi Oq",
+        price: "8 490 у.е.",
+        location: "Ташкент, Мирабадский район",
     },
     {
         img: "https://apollo-olx.cdnvideo.ru/v1/files/bl3l7ztmi5jg1-UZ/image;s=644x461",
@@ -55,9 +58,10 @@ const homedata = [
         location: "Ташкент, Юнусабадский район Вчера 12:05",
     },
     {
-        img: "https://apollo-olx.cdnvideo.ru/v1/files/bl3l7ztmi5jg1-UZ/image;s=644x461",
-        title: "Chinoz Qum, Pesok, Чиназский песок Turpoq",
-        location: "Ташкент, Юнусабадский район Вчера 12:05",
+        img: "https://apollo-olx.cdnvideo.ru/v1/files/t0n6cu5vo7p53-UZ/image;s=644x461",
+        title: "участок 2,1 сот. и половина углового дома у метро Буюк Ипак Йули",
+        price: "75 000 у.е.",
+        location: "Ташкент, Мирзо-Улугбекский район 31 июль",
     },
     {
         img: "https://apollo-olx.cdnvideo.ru/v1/files/bl3l7ztmi5jg1-UZ/image;s=644x461",
@@ -65,9 +69,10 @@ const homedata = [
         location: "Ташкент, Юнусабадский район Вчера 12:05",
     },
     {
-        img: "https://apollo-olx.cdnvideo.ru/v1/files/bl3l7ztmi5jg1-UZ/image;s=644x461",
-        title: "Chinoz Qum, Pesok, Чиназский песок Turpoq",
-        location: "Ташкент, Юнусабадский район Вчера 12:05",
+        img: "https://apollo-olx.cdnvideo.ru/v1/files/i12xggyis13j3-UZ/image;s=644x461",
+        title: "Ортопедические и анатомические матраси от производителя.",
+        price: "300 000 сум",
+        location: "Ташкент, Юнусабадский район 31 июль",
     },
     {
         img: "https://apollo-olx.cdnvideo.ru/v1/files/bl3l7ztmi5jg1-UZ/image;s=644x461",
